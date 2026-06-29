64a1ec553af370655d329cafe452b1fbc705371a
albaspazio <alberto.inuggi@gmail.com>
2026-06-29 14:13:21 +0200

    version 1.4.0 moved to libs.versions.toml added codegraph ignore

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id(Plugins.androidLibrary) version Versions.androidLibrary apply(false)
-    id(Plugins.kotlinAndroid) version Versions.kotlin apply(false)
-    id(Plugins.kotlinParcelize) version Versions.kparcelablePlugin apply(false)
+    id(libs.plugins.android.library.get().pluginId) version libs.versions.androidGradlePlugin.get() apply(false)
+    id(libs.plugins.kotlin.android.get().pluginId) version libs.versions.kotlinPlugin.get() apply(false)
+    id(libs.plugins.kotlin.parcelize.get().pluginId) version libs.versions.kotlinPlugin.get() apply(false)
 }
 
 tasks.register("clean", Delete::class){
--- buildSrc/src/main/kotlin/dependencies.kt
+++ buildSrc/src/main/kotlin/dependencies.kt
@@ -1,5 +1,4 @@
 object Configs {
-
     const val corenamespace     = "org.albaspazio.core"
 
     const val compileSdkVersion = 34
@@ -7,85 +6,7 @@ object Configs {
     const val targetSdkVersion  = 26
 }
 
-object Plugins {
-
-    const val androidLibrary        = "com.android.library"
-    const val kotlinAndroid         = "org.jetbrains.kotlin.android"
-    const val kotlinParcelize       = "org.jetbrains.kotlin.plugin.parcelize"
-}
-
-object Versions {
-
-    const val kotlin = "1.9.22"           // Required for modern metadata
-    const val androidLibrary = "8.2.2"    // Fixes the 'IncrementalTaskInputs' crash
-    const val lifecycle = "2.6.1"         // Updated for better compatibility
-    const val ktxCore = "1.12.0"
-    const val appCompat = "1.6.1"
-    const val kparcelablePlugin = "1.7.0"
-    const val constraintLayout = "2.1.4"
-    const val material = "1.6.1"
-    const val localbroadcastmanager = "1.1.0"
-    const val navFragment = "2.3.5"
-    const val moshi = "1.12.0"
-    const val rxkotlin = "2.4.0"
-    const val rxandroid = "2.1.1"
-    const val sunmail = "1.6.7"
-    const val okhttp = "4.12.0"
-
-    const val junit = "4.13.2"
-    const val testRunner = "1.5.2"
-    const val testEspressoCore = "3.5.1"
-
-}
-
-object Dependencies {
-
-    object AndroidX {
-        const val lifecycleviewmodel= "androidx.lifecycle:lifecycle-viewmodel-ktx:${Versions.lifecycle}"
-        const val navFragment       = "androidx.navigation:navigation-fragment-ktx:${Versions.navFragment}"
-        const val navUi             = "androidx.navigation:navigation-ui-ktx:${Versions.navFragment}"
-        const val ktxCore           = "androidx.core:core-ktx:${Versions.ktxCore}"
-        const val appCompat         = "androidx.appcompat:appcompat:${Versions.appCompat}"
-        const val constraintLayout  = "androidx.constraintlayout:constraintlayout:${Versions.constraintLayout}"
-        const val material          = "com.google.android.material:material:${Versions.material}"
-
-        const val lifecycledataKtx  = "androidx.lifecycle:lifecycle-livedata-ktx:${Versions.lifecycle}"
-        const val lifecyclecommon   = "androidx.lifecycle:lifecycle-common-java8:${Versions.lifecycle}"
-        const val localbroadcastmanager   = "androidx.localbroadcastmanager:localbroadcastmanager:${Versions.localbroadcastmanager}"
-
-        const val testRunner        = "androidx.test:runner:${Versions.testRunner}"
-        const val testEspressoCore  = "androidx.test.espresso:espresso-core:${Versions.testEspressoCore}"
-    }
-
-    object Kotlin {
-        const val stdLib    = "org.jetbrains.kotlin:kotlin-stdlib:${Versions.kotlin}"
-        const val reflect   = "org.jetbrains.kotlin:kotlin-reflect:${Versions.kotlin}"
-    }
-
-    object Moshi {
-        const val moshi = "com.squareup.moshi:moshi:${Versions.moshi}"
-        const val moshiKt = "com.squareup.moshi:moshi-kotlin:${Versions.moshi}"
-    }
-
-    object rx {
-        const val rxandroid = "io.reactivex.rxjava2:rxandroid:${Versions.rxandroid}"
-        const val rxrelay   = "com.jakewharton.rxrelay2:rxrelay:${Versions.rxandroid}"
-        const val rxkotlin  = "io.reactivex.rxjava2:rxkotlin:${Versions.rxkotlin}"
-    }
-
-    object sunmail {
-        const val mail          = "com.sun.mail:android-mail:${Versions.sunmail}"
-        const val activation    = "com.sun.mail:android-activation:${Versions.sunmail}"
-    }
-
-    object network {
-        const val okhttp = "com.squareup.okhttp3:okhttp:${Versions.okhttp}"
-    }
-
-    const val junit             = "junit:junit:${Versions.junit}"
-}
-
 object ProGuards {
-    val androidDefault = "proguard-rules.pro"
-    val proguardTxt = "proguard-android.txt"
+    const val androidDefault = "proguard-rules.pro"
+    const val proguardTxt = "proguard-android.txt"
 }
